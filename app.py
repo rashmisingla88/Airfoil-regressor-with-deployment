fcd4082f7eea71d0052ce39484b2f59bc60eb5a3
rashmisingla88 <103103983+rashmisingla88@users.noreply.github.com>
2023-08-19 13:27:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import pandas as pd
 app = Flask(__name__)
 model = pickle.load(open('bestmodel.pkl', 'rb'))
 
-@app.route('/home')
+@app.route('/')
 def home():
     return render_template('home.html')
 
